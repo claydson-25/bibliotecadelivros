fb915e131ced391311a050b5a4ef68e3885128d5
claydson-25 <85529032+claydson-25@users.noreply.github.com>
2021-07-14 01:30:15 -0300

    Tarefa 4

--- livros.c
+++ livros.c
@@ -223,9 +223,9 @@ void telaModuloRelatorio(void)
     printf("///            = = = = = Sistema de Biblioteca de Livros = = = = =          ///\n");
     printf("///                                                                         ///\n");
     printf("///                       4. Módulo Relatório                               ///\n");
-    printf("///                       a. Relatório de Livros Cadastrado                 ///\n");
+    printf("///                       a. Relatório de Livros Cadastrados                ///\n");
     printf("///                       b. Relatório de Livros Emprestados                ///\n");
-    printf("///                       c Relatório de Clientes Ativos                   ///\n");
+    printf("///                       c Relatório de Clientes Ativos                    ///\n");
     printf("///                       d. Relatório de Empréstimo                        ///\n");
     printf("///                       e. Relatório dos Empréstimos em Atraso            ///\n");
     printf("///                       0. Sair                                           ///\n");
@@ -321,5 +321,259 @@ void ModuloRelatorio(void)
 }
 
 
+void ModuloLivro(void)
+{
+    char opçao;
+    do{
+        opçao= ModuloLivro();
+        switch(opçao){
+                case '1': CadastrarLivro   ();
+                                    break;
+                case '2': PesquisarLivro   ();
+                                    break;
+                case '3': AlterarLivro     ();
+                                    break;
+                case '4': ExcluirLivro     ();
+                                    break;
+                case '0': Sair             ();
+                                    break;
+        }
+
+    } while (opçao !=0);
+    printf('The End\n');
+}
+
+void CadastrarLivro(void)
+{
+    Livro* liv;
+
+    liv= telaCadastrarLivro();
+    gravarLivro(liv);
+    free(liv);
+
+}
+
+void PesquisarLivro(void)
+{
+    Livro* liv;
+    char*  isbn;
+    
+    isbn= telaPesquisarLivro()
+    liv=  buscarLivro(isbn);
+    exibirLivro(liv);
+    free(Isbn);
+    free(liv);
+
+}
+
+void AlterarLivro(void)
+{
+    Livro* liv;
+
+    liv= telaAlterarLivro();
+    gravarAlteraçao(liv);
+    free(liv);
+}
+
+void ExcluirLivro(void)
+{   Livro* liv;
+
+    liv= telaExcluirLivro();
+    gravarExclusao(liv);
+    free(liv);
+}
+
+void ModuloCliente(void)
+{
+    char opçao;
+    do{
+        opçao= ModuloCliente();
+        switch(opçao){
+                case '1': CadastrarCLiente  ();
+                                    break;
+                case '2': PesquisarCliente  ();
+                                    break;
+                case '3': EditarCliente     ();
+                                     break;
+                case '4': ExcluirCLiente    ();
+                                    break;
+                case '0': Sair              ();
+                                    break;
+        }
+
+    } while (opçao !=0);
+    printf('The End/n');
+}
 
 
+
+void CadastrarCliente(void)
+{
+    Cliente* clien;
+
+    clien= telaCadastrarCliente();
+    gravarCliente(clien);
+    free(clien);
+
+}
+
+void PesquisarCliente(void)
+{
+    Cliente* clien;
+    //char*  isbn;
+    
+    //isbn= telaPesquisarLivro()
+    encontrarclien(clien);
+    //free(Isbn);
+    free(clien);
+
+}
+
+void EditarCliente(void)
+{
+    Cliente* clien;
+
+    clien= telaEditarCliente();
+    gravarAlteraçao(clien);
+    free(clien);
+}
+
+void ExcluirCliente(void)
+{   Cliente* clien;
+
+    clien= telaExcluirCliente();
+    gravarExclusao(clien);
+    free(clien);
+}
+
+void ModuloEmprestimo(void)
+{
+    char opçao;
+    do{
+        opçao= ModuloEmprestimo();
+        switch(opçao){
+                case '1': CadastrarEmprestimo  ();
+                                        break;
+                case '2': PesquisarEmprestimo  ();
+                                        break;
+                case '3': EditarEmprestimo     ();
+                                        break;
+                case '4': DevolverEmprestimo   ();
+                                        break;
+                case '0': Sair                 ();
+                                        break;
+        }
+
+    } while (opçao !=0);
+    printf('The End\n');
+}
+
+void CadastrarEmprestimo(void)
+{
+    Emprest* emp;
+
+    emp= telaCadastrarEmprestimo();
+    gravarEmprestimo(emp);
+    free(emp);
+
+}
+
+void PesquisarEmprestimo(void)
+{
+    Emprest* emp;
+    
+    emp= telaPesquisarEmprestimo();
+    exibirEmprestimo(emp);
+    free(emp);
+
+}
+
+void EditarEmprestimo(void)
+{
+    Emprest* emp;
+
+    emp= telaEmprestimo();
+    gravarAlteraçao(emp);
+    free(emp);
+}
+
+void ExcluirEmprestimo(void)
+{   
+    Emprest* emp;
+
+    emp= telaExcluirEmprestimo();
+    gravarExclusao(emp);
+    free(liv);
+}
+
+void ModuloRelatorio(void)
+{
+    char opçao;
+    do{
+        opçao= ModuloRelatorio();
+        switch(opçao){
+                case '1': RelatoriodeLivrosCadastrados    ();
+                                               break;
+                case '2': RelatoriodeLivrosEmprestados    ();
+                                               break;
+                case '3': RelatoriodeClientesAtivos       ();
+                                               break;
+                case '4': RelatoriodeDeEmprestimos        ();
+                                               break;
+                case '5': RelatoriodeDeEmprestimosemAtraso();
+                                               break; 
+                case '0': Sair                            ();
+                                               break;
+        }
+
+    } while (opçao !=0);
+    printf('The End\n');
+}
+
+void RelatoriodeLivrosCadastrado(void)
+{
+    Livros* cadast;
+
+    cadast= teladeLivroCadastrado();
+    mostrarcadastro(cadast);
+    free(cadast);
+
+}
+
+void RelatoriodeLivrosEmprestados(void)
+{
+    Livros* cadast;
+    
+    cadast= teladelivrosEmprestados();
+    mostraremprestimo(cadast);
+    free(cadast);
+
+}
+
+void RelatoriodeClientesAtivos(void)
+{
+    Livro* cadast;
+
+    cadast= teladeClientesAtivos();
+    mostrarclientes(cadast);
+    free(cadast);
+}
+
+void RelatoriodeEmprestimo(void)
+{   Livro* cadast;
+
+    cadast= telaRelatoriodeEmprestimo();
+    mostraremprestimo(cadast);
+    free(cadast);
+}
+
+void RelatoriodeEmprestimoemAtraso(void)
+{   Livro* cadast;
+
+    cadast= telaRelatoriodeEmprestimoemAtraso();
+    mostraremprestimoematraso(cadast);
+    free(cadast);
+}
+
+
+ 
