24ac1b8be9d303a56ac1f6eb1cdf577a495394cb
claydson-25 <85529032+claydson-25@users.noreply.github.com>
2021-09-13 09:43:19 -0300

    Tarefa

--- valida.c
+++ valida.c
@@ -168,3 +168,43 @@ int validarCPF(char *cpf)
     }
     return 1;
 }
+
+
+int validarISBN(char *isbn)
+{
+    int i, j, digito1 = 0, digito2 = 0;
+    if(strlen(isbn) != 11)
+        return 0;
+    else if((strcmp(isbn,"00000000000") == 0) || (strcmp(isbn,"11111111111") == 0) || (strcmp(isbn,"22222222222") == 0) ||
+            (strcmp(isbn,"33333333333") == 0) || (strcmp(isbn,"44444444444") == 0) || (strcmp(isbn,"55555555555") == 0) ||
+            (strcmp(isbn,"66666666666") == 0) || (strcmp(isbn,"77777777777") == 0) || (strcmp(isbn,"88888888888") == 0) ||
+            (strcmp(isbn,"99999999999") == 0))
+        return 0; ///se o ISBN tiver todos os números iguais ele é inválido.
+    else
+    {
+        ///digito 1---------------------------------------------------
+        for(i = 0, j = 10; i < strlen(isbn)-2; i++, j--) ///multiplica os números de 10 a 2 e soma os resultados dentro de digito1
+            digito1 += (isbn[i]-48) * j;
+        digito1 %= 11;
+        if(digito1 < 2)
+            digito1 = 0;
+        else
+            digito1 = 11 - digito1;
+        if((isbn[9]-48) != digito1)
+            return 0; ///se o digito 1 não for o mesmo que o da validação ISBN é inválido
+        else
+        ///digito 2--------------------------------------------------
+        {
+            for(i = 0, j = 11; i < strlen(isbn)-1; i++, j--) ///multiplica os números de 11 a 2 e soma os resultados dentro de digito2
+                    digito2 += (isbn[i]-48) * j;
+        digito2 %= 11;
+        if(digito2 < 2)
+            digito2 = 0;
+        else
+            digito2 = 11 - digito2;
+        if((isbn[10]-48) != digito2)
+            return 0; ///se o digito 2 não for o mesmo que o da validação ISBN é inválido
+        }
+    }
+    return 1;
+}
